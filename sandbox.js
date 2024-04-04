32390ef292df666bfecaafce23f61179ef30ef97
Foshati <amirrezafoshati@gmail.com>
2024-04-04 23:37:27 +0330

    add typescript

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,6 @@
-var charc = 'mahdi';
-console.log(charc);
-var inputs = document.querySelectorAll('input');
+var fa = "mahdi";
+console.log(fa);
+var inputs = document.querySelectorAll("input");
 console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
